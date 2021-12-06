f3a34cbd4f0fd2ea0996182a6fb3a4180742e7a4
Paul Glorioso <goldcansio@gmail.com>
2021-12-06 12:39:09 -0800

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 12;
+var height = 6;
+var area = width * height;
+console.log('The value of area is:', area);
+console.log('The typeof of area is:', typeof area);
+
+var bill = 1200;
+var payment = 1250;
+var change = payment - bill;
+console.log('The value of change:', change);
+console.log('The typeof of change is:', typeof change);
+
+var quizzes = 79;
+var midterm = 88;
+var final = 82;
+var grade = (quizzes + midterm + final) / 3;
+console.log('The value of grade:', grade);
+console.log('The typeof of grade is:', typeof grade);
+
+var firstName = 'Paul';
+var lastName = 'Glorioso';
+var fullName = firstName + ' ' + lastName;
+console.log('The value of fullName is:', fullName);
+console.log('The typeof of fullname is:', typeof fullName);
+
+var ph = 5;
+var isAcidic = ph < 7;
+console.log('The value of isAcidic is:', isAcidic);
+console.log('The typeof of isAcidic is:', typeof isAcidic);
+
+var headCount = 200;
+var isSparta = headCount === 300;
+console.log('The value of isSparta is:', isSparta);
+console.log('The typeof of isSparta is:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('The value of motto is:', motto);
+console.log('The typeof of motto is', typeof motto);
